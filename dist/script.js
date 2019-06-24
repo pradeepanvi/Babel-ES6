cbe695064e096f40dc7d59a987e4d3f9244ce275
pradeepanvi <pradeepkumar.anvi@gmail.com>
2019-06-24 20:55:37 +0530

    Add files via upload
    ".babelrc" file will convert ES6 code to ES5

---
+++ dist/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// unnamed
+var Rectangle = function Rectangle(height, width) {
+  _classCallCheck(this, Rectangle);
+
+  this.height = height;
+  this.width = width;
+};
+
+console.log(Rectangle.name); // output: "Rectangle"
+//# sourceMappingURL=script.js.map
